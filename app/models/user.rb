8b375f9042943e5a13e00355bba132f8aaff5a12
Diana <diasandia@users.noreply.github.com>
2018-02-11 19:52:14 -0800

    Add elasticsearch to gemfile and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
+require 'elasticsearch/model'
+
 class User < ApplicationRecord
+	include Elasticsearch::Model
+
 	has_many :user_companies
 	has_many :companies, through: :user_companies
+
+	def as_indexed_json
+	  self.as_json({
+	    only: [:id, :first_name, :last_name, :email, :job_title],
+	  })
+	end
 end
